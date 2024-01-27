aad330309374f8db189049188416dbfaae75402b
Joao-Victor-KDE <joaovictorxl120@gmail.com>
2024-01-27 17:55:29 -0300

    contact validations

--- frontend/main.js
+++ frontend/main.js
@@ -7,7 +7,8 @@ import ContatoValidation from './modules/ContatoValidation';
 const cadastro = new LoginValidation('.form-cadastro');
 const login = new LoginValidation('.form-login');
 
-const contato = new ContatoValidation('.')
+const contato = new ContatoValidation('.form-contato');
 
 login.init();
 cadastro.init();
+contato.init();
--- frontend/modules/ContatoValidation.js
+++ frontend/modules/ContatoValidation.js
@@ -3,16 +3,48 @@ import validator from "validator";
 export default class ContatoVal {
     constructor(formClass) {
         this.form = document.querySelector(formClass);
+        console.log('ok')
     }
 
     init() {
         this.events();
+        console.log('init on')
     }
 
     events() {
         if(!this.form) return;
         this.form.addEventListener('submit', e=> {
             e.preventDefault();
+            this.validate(e);
         });
+    }
+
+    validate(e) { // Front-End Validation
+        console.log('Started....')
+        const element = e.target;
+        const nomeInput = element.querySelector('input[name="nome"]').value;
+        const telefoneInput = element.querySelector('input[name="telefone"]').value;
+        const emailInput = element.querySelector('input[name="email"]').value;
+        let errors = [];
+
+        if(!nomeInput){
+            errors.push('- Nome é um campo obrigatório.')
+        }
+        
+        if(!validator.isEmail(emailInput)) {
+            errors.push('- Email inválido.');
+        };
+
+        if(/[a-zA-Z]/.test(telefoneInput)) {
+            errors.push('- Telefone deve conter apenas números.')
+        };
+
+        if(!emailInput && !telefoneInput){
+            errors.push('- Pelo menos um dado de contato deve ser inserido (Email ou telefone).')
+        }
+
+        if(errors.length>0){
+            alert(`Impossível executar ação:\n\n${errors.join("\n")}`);
+        } else element.submit();
     }
 }
